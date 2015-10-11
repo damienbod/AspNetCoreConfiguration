37668e65ebf9d409e13e6186afc299d1295cb6b7
damienbod <damien_bod@hotmail.com>
2015-10-11 13:32:04 +0200

    init

---
+++ src/AspNet5Configuration/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace AspNet5Configuration.Controllers
+{
+    [Route("api/[controller]")]
+    public class AboutController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Demo showing the about for this controller" };
+        }
+
+   
+
+      
+
+    }
+}
